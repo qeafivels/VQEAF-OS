686d89d12c22057340bfff7e86ae63c55375ffb1
qeafivels <dxhop96@gmail.com>
2026-09-26 08:41:06 +0700

    feat(browser): advance smooth document and overview animation from OS loop

--- src/main.cpp
+++ src/main.cpp
@@ -883,6 +883,7 @@ void loop() {
   }
 #endif
   galleryApp.tick(appCtx, screen == ScreenId::Gallery && !osBackConfirm.active());
+  browserApp.tick(appCtx, screen == ScreenId::Browser && !osBackConfirm.active());
   wifiApp.tick(appCtx, screen == ScreenId::WiFi);
   systemService.update(notifications);
   updateWirelessNotifications();
